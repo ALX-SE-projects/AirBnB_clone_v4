b31f76cc6c263e829bae0752355b395894f554d4
Hasona <hasona@kali.kali>
2024-05-03 23:47:21 +0300

    first add

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    const amenities = {};
+    $('input[type="checkbox"]').change(function () {
+        if ($(this).is(':checked')) {
+        amenities[$(this).parent().data('id')] = $(this).parent().data('name');
+        } else {
+        delete amenities[$(this).parent().data('id')];
+        }
+        const names = Object.values(amenities).join(', ');
+        $('.amenities h4').text("Amenities: " + names);
+    }
+    );
+});
